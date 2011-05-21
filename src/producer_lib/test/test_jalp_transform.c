5b132e514432f1f6b2d9618a1e34f1686c3a30ee
John Frens <jfrens@tresys.com>
2011-05-20 23:17:04 -0400

    bug-11941 and 11943: unit tests for jalp_transform_append_aes and deflate

--- src/producer_lib/test/test_jalp_transform.c
+++ src/producer_lib/test/test_jalp_transform.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <test-dept.h>
 #include <jalop/jalp_journal_metadata.h>
+#include "jal_alloc.h"
+#include "jal_asprintf_internal.h"
+#include "jalp_base64_internal.h"
 
 void test_jalp_transform_append_return_new_transform_when_prev_is_null()
 {
@@ -104,3 +107,150 @@ void test_jalp_transform_destroy_destroys_multinode_transform_list()
 	assert_equals((struct jalp_transform *)NULL, frst_transform);
 }
 
+void test_jalp_transform_append_deflate_appends_deflate()
+{
+	struct jalp_transform *deflate_transform;
+	struct jalp_transform *deflate_transform2;
+	deflate_transform = jalp_transform_append_deflate(NULL);
+
+	deflate_transform2 = jalp_transform_append_deflate(deflate_transform);
+	assert_not_equals(NULL, deflate_transform);
+	assert_not_equals(NULL, deflate_transform2);
+	assert_equals(deflate_transform->next, deflate_transform2);
+	assert_equals((struct jalp_transform *)NULL, deflate_transform2->next);
+
+	jalp_transform_destroy(&deflate_transform);
+}
+
+void test_jalp_transform_append_deflate_has_correct_fields()
+{
+	struct jalp_transform *deflate_transform;
+	deflate_transform = jalp_transform_append_deflate(NULL);
+	assert_string_equals("http://www.dod.mil/algorithms/compression#deflate", deflate_transform->uri);
+	assert_equals((char *)NULL, deflate_transform->xml);
+	jalp_transform_destroy(&deflate_transform);
+}
+
+void test_jalp_transform_append_aes_inval_key_size()
+{
+	struct jalp_transform *invalid_aes_transform;
+	invalid_aes_transform = jalp_transform_append_aes(NULL, -50, NULL, NULL);
+	assert_equals((struct jalp_transform *)NULL, invalid_aes_transform);
+}
+
+void test_jalp_transform_append_aes_null_key_and_iv()
+{
+	struct jalp_transform *valid_aes_transform;
+	valid_aes_transform = jalp_transform_append_aes(NULL, JALP_AES128, NULL, NULL);
+	assert_not_equals(NULL, valid_aes_transform);
+	jalp_transform_destroy(&valid_aes_transform);
+}
+
+void test_jalp_transform_append_aes_appends_to_null()
+{
+	uint8_t *key;
+	uint8_t *iv;
+	unsigned int i[32];
+	unsigned int j[32];
+	key = (uint8_t *)i;
+	iv = (uint8_t *)j;
+	struct jalp_transform *aes_transform;
+	aes_transform = jalp_transform_append_aes(NULL, JALP_AES128, key, iv);
+	assert_not_equals(NULL, aes_transform);
+	jalp_transform_destroy(&aes_transform);
+}
+
+void test_jalp_transform_append_aes_appends_to_prev()
+{
+	struct jalp_transform *first = jalp_transform_append(NULL, "uri", "xml");
+	uint8_t *key;
+	uint8_t *iv;
+	unsigned int i[32];
+	unsigned int j[32];
+	key = (uint8_t *)i;
+	iv = (uint8_t *)j;
+	struct jalp_transform *aes_transform;
+	aes_transform = jalp_transform_append_aes(first, JALP_AES128, key, iv);
+	assert_not_equals(NULL, aes_transform);
+	assert_equals(first->next, aes_transform);
+	jalp_transform_destroy(&first);
+}
+
+void test_jalp_transform_append_aes_correctly_encodes_data()
+{
+	int i;
+	struct jalp_transform *aes_transform;
+	uint8_t key[128];
+	uint8_t iv[128];
+
+	/* fill key and iv with arbitrary data */
+	for(i = 0; i < 128; i++) {
+		key[i] = (uint8_t)i;
+		iv[i] = (uint8_t)(128 - i);
+	}
+
+	aes_transform = jalp_transform_append_aes(NULL, JALP_AES128, key, iv);
+
+	unsigned char *char_key = jal_malloc(128);
+	unsigned char *char_iv = jal_malloc(128);
+	memcpy(char_key, key, 128);
+	memcpy(char_iv, iv, 128);
+
+	char *b64_key = jalp_base64_enc(char_key, 128);
+	char *b64_iv = jalp_base64_enc(char_iv, 128);
+
+	char *xml_snippet;
+
+	jal_asprintf(&xml_snippet, "<Key128>%s</Key128><IV128>%s</IV128>", b64_key, b64_iv);
+
+	assert_equals(0, strcmp(xml_snippet, aes_transform->xml));
+	assert_string_equals(aes_transform->uri, "http://www.w3.org/2001/04/xmlenc#aes128-cbc");
+
+	free(xml_snippet);
+	jalp_transform_destroy(&aes_transform);
+}
+
+void test_jalp_transform_append_aes_works_with_different_keysizes()
+{
+	int i;
+	struct jalp_transform *aes_transform_128;
+	struct jalp_transform *aes_transform_192;
+	struct jalp_transform *aes_transform_256;
+	uint8_t key[256];
+
+	/* fill key and iv with arbitrary data */
+	for(i = 0; i < 256; i++) {
+		key[i] = (uint8_t)i;
+	}
+
+	aes_transform_128 = jalp_transform_append_aes(NULL, JALP_AES128, key, NULL);
+	aes_transform_192 = jalp_transform_append_aes(NULL, JALP_AES192, key, NULL);
+	aes_transform_256 = jalp_transform_append_aes(NULL, JALP_AES256, key, NULL);
+
+	unsigned char *char_key = jal_malloc(256);
+	memcpy(char_key, key, 256);
+
+	char *b64_key_128 = jalp_base64_enc(char_key, 128);
+	char *b64_key_192 = jalp_base64_enc(char_key, 192);
+	char *b64_key_256 = jalp_base64_enc(char_key, 256);
+
+	char *xml_snippet_128;
+	char *xml_snippet_192;
+	char *xml_snippet_256;
+
+	jal_asprintf(&xml_snippet_128, "<Key128>%s</Key128>", b64_key_128);
+	jal_asprintf(&xml_snippet_192, "<Key192>%s</Key192>", b64_key_192);
+	jal_asprintf(&xml_snippet_256, "<Key256>%s</Key256>", b64_key_256);
+
+	assert_equals(0, strcmp(xml_snippet_128, aes_transform_128->xml));
+	assert_equals(0, strcmp(xml_snippet_192, aes_transform_192->xml));
+	assert_equals(0, strcmp(xml_snippet_256, aes_transform_256->xml));
+
+	free(xml_snippet_128);
+	free(xml_snippet_192);
+	free(xml_snippet_256);
+
+	jalp_transform_destroy(&aes_transform_128);
+	jalp_transform_destroy(&aes_transform_192);
+	jalp_transform_destroy(&aes_transform_256);
+}
