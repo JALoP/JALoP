c62a12359c66f69f0d043996fb62abf55c2956d6
John Frens <jfrens@tresys.com>
2011-06-07 11:49:35 -0400

    bug 11986: unit tests for 11938 jalp_base64_enc

---
+++ src/producer_lib/test/test_jalp_base64.c
@@ -0,0 +1,28 @@
+#include <test-dept.h>
+#include <stdlib.h>
+#include "jalp_alloc.h"
+#include "jalp_base64_internal.h"
+
+void test_jalp_base64_enc_null_input()
+{
+	char *inval = jalp_base64_enc((unsigned char *)NULL, 10);
+	assert_equals((char *)NULL, inval);
+}
+
+void test_jalp_base64_enc_bad_lengths()
+{
+	char *inval = jalp_base64_enc((unsigned char *)"inval", 0);
+	assert_equals((char *)NULL, inval);
+
+	inval = jalp_base64_enc((unsigned char *)"inval", -200);
+	assert_equals((char *)NULL, inval);
+}
+
+void test_jalp_base64_enc_encodes()
+{
+	unsigned char bytes[4] = {(unsigned char)'a', (unsigned char)'s',
+				(unsigned char)'d', (unsigned char)'f'};
+	char *valid = jalp_base64_enc(bytes, 4);
+	assert_string_equals(valid, "YXNkZg==");
+	free(valid);
+}
