7f474330b6ab1109a026f8a4c2e97897efd6535a
Phil Black-Knight <pblack@tresys.com>
2011-06-07 10:36:08 -0400

    bug-12182: unit tests for jalp_contxt_destroy

--- src/producer_lib/test/test_jalp_context.c
+++ src/producer_lib/test/test_jalp_context.c
@@ -1,7 +1,27 @@
 #include <test-dept.h>
 #include <jalop/jalp_context.h>
+#include "jal_alloc.h"
 #include "jalp_context_internal.h"
-
+#include <unistd.h>
+#define FAKE_SOCKET (int)0xdeadbeef
+static int close_called;
+int mocked_close(int fd)
+{
+	if (fd == FAKE_SOCKET) {
+		close_called = 1;
+		return 0;
+	}
+	return close(fd);
+}
+void setup()
+{
+	close_called = 0;
+}
+void teardown()
+{
+	close_called = 0;
+	restore_function(close);
+}
 void test_jalp_context_create_returns_struct_with_zeroed_fields()
 {
 	struct jalp_context_t *ptr = jalp_context_create();
@@ -12,5 +32,35 @@ void test_jalp_context_create_returns_struct_with_zeroed_fields()
 	assert_equals((char *) NULL, ptr->hostname);
 	assert_equals((char *) NULL, ptr->app_name);
 	jalp_context_destroy(&ptr);
+}
+
+void test_jalp_context_destroy_does_not_crash_with_null()
+{
+	jalp_context_destroy(NULL);
+	struct jalp_context_t *ptr = NULL;
+	jalp_context_destroy(&ptr);
+	assert_equals((void *) NULL, ptr);
+}
+
+void test_jalp_context_destroy_closes_socket()
+{
+	replace_function(close, mocked_close)
+	struct jalp_context_t *ptr = jalp_context_create();
+	// bogus file descriptor
+	ptr->socket = FAKE_SOCKET;
+
+	jalp_context_destroy(&ptr);
+	assert_equals((void *) NULL, ptr);
+	assert_equals(1, close_called);
+}
+void test_jalp_context_destroy_release_memory()
+{
+	// test under valgrind
+	struct jalp_context_t *ptr = jalp_context_create();
+	ptr->path = jal_strdup("/foo/bar");
+	ptr->app_name = jal_strdup("test-dept");
+
+	jalp_context_destroy(&ptr);
+	assert_equals((void *) NULL, ptr);
 }
 
