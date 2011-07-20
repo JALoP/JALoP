319320f6043eabc13b4f7f1829b6578cfadff6dd
Phil Black-Knight <pblack@tresys.com>
2011-07-20 09:31:49 -0400

    bug-11089: adding jalp_journal_path()

--- src/producer_lib/src/jalp_journal.cpp
+++ src/producer_lib/src/jalp_journal.cpp
@@ -43,6 +43,9 @@
 #include "jalp_app_metadata_xml.hpp"
 #include "jalp_xml_utils.hpp"
 #include "jalp_digest_internal.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 XERCES_CPP_NAMESPACE_USE
 
 static const XMLCh JALP_XML_CORE[] = {	chLatin_C,
@@ -133,3 +136,89 @@ out:
 	}
 	return status;
 }
+enum jal_status jalp_journal(jalp_context *ctx, 
+		struct jalp_app_metadata *app_meta,
+		const uint8_t *journal_buffer,
+		const size_t journal_buffer_size)
+{
+	enum jal_status status;
+	uint8_t *digest = NULL;
+	DOMImplementation *impl = NULL;
+	DOMElement *app_meta_elem = NULL;
+	DOMDocument *doc = NULL;
+	MemBufFormatTarget *buffer = NULL;
+
+	if (!ctx || !journal_buffer || (journal_buffer_size == 0)) {
+		return JAL_E_INVAL;
+	}
+
+	if (app_meta) {
+		impl = DOMImplementationRegistry::getDOMImplementation(JALP_XML_CORE);
+		doc = impl->createDocument();
+		status = jalp_app_metadata_to_elem(app_meta, ctx, NULL, doc, &app_meta_elem);
+		if (status != JAL_OK) {
+			goto out;
+		}
+		if (ctx->digest_ctx) {
+			status = jalp_digest_buffer(ctx->digest_ctx,
+					journal_buffer, journal_buffer_size,
+					&digest);
+			if (status != JAL_OK) {
+				goto out;
+			}
+			DOMElement *reference_elem = NULL;
+			status = jal_create_reference_elem(PAYLOAD_URI, ctx->digest_ctx->algorithm_uri,
+					digest, ctx->digest_ctx->len, doc, &reference_elem);
+			if (status != JAL_OK) {
+				goto out;
+			}
+
+			XMLCh *namespace_uri = XMLString::transcode(JALP_XMLDSIG_URI);
+			DOMElement *manifest = doc->createElementNS(namespace_uri, JALP_XML_MANIFEST);
+			XMLString::release(&namespace_uri);
+			manifest->appendChild(reference_elem);
+			app_meta_elem->appendChild(manifest);
+		}
+		if (ctx->signing_key) {
+			// TODO: sign the doc
+		}
+		status = jal_xml_output(doc, &buffer);
+		if (status != JAL_OK) {
+			goto out;
+		}
+		status = jalp_send_buffer(ctx, JALP_JOURNAL_MSG,
+			(void*) journal_buffer, journal_buffer_size,
+			(void*) buffer->getRawBuffer(), buffer->getLen(), -1);
+	} else {
+		status = jalp_send_buffer(ctx, JALP_JOURNAL_MSG,
+			(void*) journal_buffer, journal_buffer_size,
+			NULL, 0, -1);
+	}
+
+out:
+	if (buffer) {
+		delete buffer;
+	}
+	free(digest);
+	if (doc) {
+		doc->release();
+	}
+	return status;
+}
+
+enum jal_status jalp_journal_path(jalp_context *ctx,
+		struct jalp_app_metadata *app_meta,
+		const char *path)
+{
+	if (!ctx || !path) {
+		return JAL_E_INVAL;
+	}
+	int fd = open(path, O_RDONLY);
+	if (fd == -1) {
+		return JAL_E_INVAL;
+	}
+	enum jal_status ret = jalp_journal_fd(ctx, app_meta, fd);
+	close(fd);
+	return ret;
+}
+
