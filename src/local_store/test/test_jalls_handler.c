f88dfa26c72a83571018ce4b01b397ba5c80b98d
Jason Dana <jdana@tresys.com>
2011-08-19 10:30:41 -0400

    bug-12655: unit tests for local store work thread

---
+++ src/local_store/test/test_jalls_handler.c
@@ -0,0 +1,276 @@
+/**
+ * @file test_jalls_handler.c This file contains tests for jalls_handler functions.n
+ *
+ * @section LICENSE
+ *
+ * Source code in 3rd-party is licensed and owned by their respective
+ * copyright holders.
+ *
+ * All other source code is copyright Tresys Technology and licensed as below.
+ *
+ * Copyright (c) 2011 Tresys Technology LLC, Columbia, Maryland, USA
+ *
+ * This software was developed by Tresys Technology LLC
+ * with U.S. Government sponsorship.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <test-dept.h>
+#include "jal_alloc.h"
+#include "jalls_msg.h"
+#include "jalls_handler.h"
+
+#define FAKE_MSG_SIZE 128
+
+static int pthread_detach_always_fails(__attribute__((unused)) pthread_t thread)
+{
+	return -1;
+}
+
+static int fake_pthread_detach(__attribute((unused)) pthread_t thread)
+{
+	return 0;
+}
+
+static pthread_t fake_pthread_self(void)
+{
+	return 0;
+}
+
+static int recvmsg_returns_msg_type_zero(__attribute__((unused)) int fd,
+					struct msghdr *msg,
+					__attribute__((unused)) int flags)
+{
+	*(uint16_t *)msg->msg_iov[0].iov_base = 1;
+	*(uint16_t *)msg->msg_iov[1].iov_base = 0;
+	*(uint16_t *)msg->msg_iov[2].iov_base = 0;
+	*(uint16_t *)msg->msg_iov[3].iov_base = 0;
+
+	msg->msg_control = NULL;
+
+	return FAKE_MSG_SIZE;
+}
+
+static int recvmsg_returns_msg_type_jalls_log_msg(__attribute__((unused)) int fd,
+							struct msghdr *msg,
+							__attribute__((unused)) int flags)
+{
+	*(uint16_t *)msg->msg_iov[0].iov_base = 1;
+	*(uint16_t *)msg->msg_iov[1].iov_base = 1;
+	*(uint16_t *)msg->msg_iov[2].iov_base = 0;
+	*(uint16_t *)msg->msg_iov[3].iov_base = 0;
+
+	msg->msg_control = NULL;
+
+	return FAKE_MSG_SIZE;
+}
+
+static int recvmsg_returns_msg_type_jalls_audit_msg(__attribute__((unused)) int fd,
+						struct msghdr *msg,
+						__attribute__((unused)) int flags)
+{
+	*(uint16_t *)msg->msg_iov[0].iov_base = 1;
+	*(uint16_t *)msg->msg_iov[1].iov_base = 2;
+	*(uint16_t *)msg->msg_iov[2].iov_base = 0;
+	*(uint16_t *)msg->msg_iov[3].iov_base = 0;
+
+	msg->msg_control = NULL;
+
+	return FAKE_MSG_SIZE;
+}
+
+static int recvmsg_returns_msg_type_jalls_journal_msg(__attribute__((unused)) int fd,
+						struct msghdr *msg,
+						__attribute__((unused)) int flags)
+{
+	*(uint16_t *)msg->msg_iov[0].iov_base = 1;
+	*(uint16_t *)msg->msg_iov[1].iov_base = 3;
+	*(uint16_t *)msg->msg_iov[2].iov_base = 0;
+	*(uint16_t *)msg->msg_iov[3].iov_base = 0;
+
+	msg->msg_control = NULL;
+
+	return FAKE_MSG_SIZE;
+}
+
+static int recvmsg_returns_msg_type_jalls_journal_fd_msg(int fd,
+							struct msghdr *msg,
+							__attribute__((unused)) int flags)
+{
+	struct cmsghdr *cmsgh = jal_malloc(sizeof(*cmsgh));
+
+	cmsgh->cmsg_len = CMSG_LEN(sizeof(fd));
+	cmsgh->cmsg_level = SOL_SOCKET;
+	cmsgh->cmsg_type = SCM_RIGHTS;
+
+	*(uint16_t *)msg->msg_iov[0].iov_base = 1;
+	*(uint16_t *)msg->msg_iov[1].iov_base = 4;
+	*(uint16_t *)msg->msg_iov[2].iov_base = 0;
+	*(uint16_t *)msg->msg_iov[3].iov_base = 0;
+
+	msg->msg_control = cmsgh;
+
+	unsigned char *data = CMSG_DATA(cmsgh);
+
+	*data = (unsigned char)fd;
+
+	return FAKE_MSG_SIZE;
+}
+
+static int recvmsg_returns_protocol_zero(__attribute__((unused)) int fd,
+					struct msghdr *msg,
+					__attribute__((unused)) int flags)
+{
+	*(uint16_t *)msg->msg_iov[0].iov_base = 0;
+	*(uint16_t *)msg->msg_iov[1].iov_base = 4;
+	*(uint16_t *)msg->msg_iov[2].iov_base = 0;
+	*(uint16_t *)msg->msg_iov[3].iov_base = 0;
+
+	msg->msg_control = NULL;
+
+	return FAKE_MSG_SIZE;
+}
+
+static int recvmsg_returns_non_null_cmsg_len_zero(__attribute__((unused)) int fd,
+							struct msghdr *msg,
+							__attribute__((unused)) int flags)
+{
+	struct cmsghdr *cmsgh = jal_malloc(sizeof(*cmsgh));
+
+	cmsgh->cmsg_len = 0;
+	cmsgh->cmsg_level = SOL_SOCKET;
+	cmsgh->cmsg_type = SCM_RIGHTS;
+
+	*(uint16_t *)msg->msg_iov[0].iov_base = 1;
+	*(uint16_t *)msg->msg_iov[1].iov_base = 4;
+	*(uint16_t *)msg->msg_iov[2].iov_base = 0;
+	*(uint16_t *)msg->msg_iov[3].iov_base = 0;
+
+	msg->msg_control = cmsgh;
+
+	unsigned char *data = CMSG_DATA(cmsgh);
+
+	*data = (unsigned char)fd;
+
+	return FAKE_MSG_SIZE;
+}
+
+static int recvmsg_always_fails(__attribute__((unused)) int fd,
+			__attribute__((unused)) struct msghdr *msg,
+			__attribute__((unused)) int flags)
+{
+	return -1;
+}
+
+struct jalls_context *jalls_ctx = NULL;
+struct jalls_thread_context *thread_ctx = NULL;
+
+void setup()
+{
+	jalls_ctx = jal_calloc(1, sizeof(*jalls_ctx));
+	jalls_ctx->debug = 0;
+
+	thread_ctx = jal_calloc(1, sizeof(*thread_ctx));
+	thread_ctx->fd = 0;
+	thread_ctx->ctx = jalls_ctx;
+
+	replace_function(pthread_self, fake_pthread_self);
+	replace_function(pthread_detach, fake_pthread_detach);
+}
+
+void teardown()
+{
+	restore_function(pthread_self);
+	restore_function(pthread_detach);
+	restore_function(jalls_recvmsg_helper);
+}
+
+void test_jalls_handler_returns_null_when_given_null()
+{
+	void *ret = jalls_handler(NULL);
+	assert_equals((void *) NULL, ret);
+}
+
+void test_jalls_handler_returns_null_when_pthread_detach_fails()
+{
+	replace_function(pthread_detach, pthread_detach_always_fails);
+	void *ret = jalls_handler(thread_ctx);
+	assert_equals((void *) NULL, ret);
+}
+
+void test_jalls_handler_returns_null_when_recvmsg_fails()
+{
+	replace_function(jalls_recvmsg_helper, recvmsg_always_fails);
+	void *ret = jalls_handler(thread_ctx);
+	assert_equals((void *) NULL, ret);
+}
+
+void test_jalls_handler_msg_type_is_zero()
+{
+	replace_function(jalls_recvmsg_helper, recvmsg_returns_msg_type_zero);
+	void *ret = jalls_handler(thread_ctx);
+	assert_equals((void *) NULL, ret);
+}
+
+void test_jalls_handler_msg_type_is_jalls_log_msg()
+{
+	replace_function(jalls_recvmsg_helper, recvmsg_returns_msg_type_jalls_log_msg);
+	void *ret = jalls_handler(thread_ctx);
+	assert_equals((void *) NULL, ret);
+}
+
+void test_jalls_handler_msg_type_is_jalls_audit_msg()
+{
+	replace_function(jalls_recvmsg_helper, recvmsg_returns_msg_type_jalls_audit_msg);
+	void *ret = jalls_handler(thread_ctx);
+	assert_equals((void *) NULL, ret);
+}
+
+void test_jalls_handler_msg_type_is_jalls_journal_msg()
+{
+	replace_function(jalls_recvmsg_helper, recvmsg_returns_msg_type_jalls_journal_msg);
+	void *ret = jalls_handler(thread_ctx);
+	assert_equals((void *) NULL, ret);
+}
+
+void test_jalls_handler_msg_type_is_jalls_journal_fd_msg()
+{
+	replace_function(jalls_recvmsg_helper, recvmsg_returns_msg_type_jalls_journal_fd_msg);
+	void *ret = jalls_handler(thread_ctx);
+	assert_equals((void *) NULL, ret);
+}
+
+void test_jalls_handler_protocol_is_zero()
+{
+	replace_function(jalls_recvmsg_helper, recvmsg_returns_protocol_zero);
+	void *ret = jalls_handler(thread_ctx);
+	assert_equals((void *) NULL, ret);
+}
+
+void test_jalls_handler_exits_cleanly_when_cmsg_len_zero_and_cmsg_not_null()
+{
+	replace_function(jalls_recvmsg_helper, recvmsg_returns_non_null_cmsg_len_zero);
+	void *ret = jalls_handler(thread_ctx);
+	assert_equals((void *) NULL, ret);
+
+}
---
+++ src/local_store/test/test_jalls_msg.c
@@ -0,0 +1,87 @@
+/**
+ * @file test_jalls_msg.c This file contains tests for jalls_msg functions.
+ *
+ * @section LICENSE
+ *
+ * Source code in 3rd-party is licensed and owned by their respective
+ * copyright holders.
+ *
+ * All other source code is copyright Tresys Technology and licensed as below.
+ *
+ * Copyright (c) 2011 Tresys Technology LLC, Columbia, Maryland, USA
+ *
+ * This software was developed by Tresys Technology LLC
+ * with U.S. Government sponsorship.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <string.h>
+#include <test-dept.h>
+#include "jalls_msg.h"
+
+#define FAKE_MSG_SIZE 128
+
+void teardown()
+{
+	restore_function(recvmsg);
+}
+
+static ssize_t fake_recvmsg(__attribute((unused)) int fd,
+			__attribute__((unused)) struct msghdr *msg,
+			__attribute__((unused)) int flags)
+{
+	return FAKE_MSG_SIZE;
+}
+
+static ssize_t recvmsg_always_fails(__attribute((unused)) int fd,
+				__attribute((unused)) struct msghdr *msg,
+				__attribute((unused)) int flags)
+{
+	return -1;
+}
+
+static ssize_t recvmsg_always_fails_errno_EBADF(__attribute((unused)) int fd,
+					__attribute((unused)) struct msghdr *msg,
+					__attribute((unused)) int flags)
+{
+	errno = EBADF;
+	return -1;
+}
+
+void test_jalls_recvmsg_returns_valid_size_given_valid_input()
+{
+	replace_function(recvmsg, fake_recvmsg);
+	ssize_t rc = jalls_recvmsg_helper(0, NULL, 0);
+	assert_equals(FAKE_MSG_SIZE, rc);
+}
+
+void test_jalls_recvmsg_returns_error_when_recvmsg_fails()
+{
+	replace_function(recvmsg, recvmsg_always_fails);
+	ssize_t rc = jalls_recvmsg_helper(0, NULL, 0);
+	assert_equals(-1, rc);
+}
+
+void test_jalls_recvmsg_returns_error_when_recvmsg_fails_with_errno_EBADF()
+{
+	replace_function(recvmsg, recvmsg_always_fails_errno_EBADF);
+	ssize_t rc = jalls_recvmsg_helper(0, NULL, 0);
+	assert_equals(EBADF, errno);
+	assert_equals(-1, rc);
+}
