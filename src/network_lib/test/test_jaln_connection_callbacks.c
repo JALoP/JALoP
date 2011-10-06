579734f5dd09b3ab4f5d45f80db2a18d77dae3dd
Caleb Case <ccase@tresys.com>
2011-10-06 13:07:27 -0400

    bug-11302: unit tests for jaln_register_connection_callbacks

--- src/network_lib/test/test_jaln_connection_callbacks.c
+++ src/network_lib/test/test_jaln_connection_callbacks.c
@@ -26,9 +26,47 @@
  * limitations under the License.
  */
 
+#include "jaln_context.h"
 #include <jalop/jaln_connection_callbacks.h>
+#include <jalop/jaln_network.h>
+#include "jaln_connection_callbacks_internal.h"
 #include <test-dept.h>
 #include <string.h>
+
+/* Dummy connection callbacks. */
+
+enum jaln_connect_error my_connect_request_handler(
+	const struct jaln_connect_request *req __attribute__((unused)),
+	int *selected_encoding __attribute__((unused)),
+	int *selected_digest __attribute__((unused)),
+	void *user_data __attribute__((unused)))
+{
+	return JALN_CE_ACCEPT;
+}
+
+void my_on_channel_close(
+	const struct jaln_channel_info *channel_info __attribute__((unused)),
+	void *user_data __attribute__((unused)))
+{
+}
+
+void my_on_connection_close(
+	const struct jaln_connection *jal_conn __attribute__((unused)),
+	void *user_data __attribute__((unused)))
+{
+}
+
+void my_connect_ack(
+	const struct jaln_connect_ack *ack __attribute__((unused)),
+	void *user_data __attribute__((unused)))
+{
+}
+
+void my_connect_nack(
+	const struct jaln_connect_nack *nack __attribute__((unused)),
+	void *user_data __attribute__((unused)))
+{
+}
 
 void test_publish_callbacks_create()
 {
@@ -45,4 +83,59 @@ void test_publish_callbacks_destroy_does_not_crash()
 	struct jaln_connection_callbacks *cb = NULL;
 	jaln_connection_callbacks_destroy(NULL);
 	jaln_connection_callbacks_destroy(&cb);
+}
+
+void test_register_connection_callbacks()
+{
+	jaln_context *ctx = jaln_context_create();
+
+	struct jaln_connection_callbacks *cb = jaln_connection_callbacks_create();
+	cb->connect_request_handler = my_connect_request_handler;
+	cb->on_channel_close = my_on_channel_close;
+	cb->on_connection_close = my_on_connection_close;
+	cb->connect_ack = my_connect_ack;
+	cb->connect_nack = my_connect_nack;
+
+	assert_equals(JAL_OK, jaln_register_connection_callbacks(ctx, cb));
+
+	jaln_context_destroy(&ctx);
+}
+
+void test_register_connection_callbacks_fails_with_invalid()
+{
+	jaln_context *ctx = jaln_context_create();
+
+	assert_equals(JAL_E_INVAL, jaln_register_connection_callbacks(ctx, NULL));
+
+	struct jaln_connection_callbacks *cb = jaln_connection_callbacks_create();
+	cb->connect_request_handler = NULL;
+	cb->on_channel_close = NULL;
+	cb->on_connection_close = NULL;
+	cb->connect_ack = NULL;
+	cb->connect_nack = NULL;
+
+	assert_equals(JAL_E_INVAL, jaln_register_connection_callbacks(ctx, cb));
+
+	cb->connect_request_handler = my_connect_request_handler;
+	assert_equals(JAL_E_INVAL, jaln_register_connection_callbacks(ctx, cb));
+
+	cb->on_channel_close = my_on_channel_close;
+	assert_equals(JAL_E_INVAL, jaln_register_connection_callbacks(ctx, cb));
+
+	cb->on_connection_close = my_on_connection_close;
+	assert_equals(JAL_E_INVAL, jaln_register_connection_callbacks(ctx, cb));
+
+	cb->connect_ack = my_connect_ack;
+	assert_equals(JAL_E_INVAL, jaln_register_connection_callbacks(ctx, cb));
+
+	/* Callback will be valid after this, testing with invalid context. */
+	cb->connect_nack = my_connect_nack;
+	assert_equals(JAL_E_INVAL, jaln_register_connection_callbacks(NULL, cb));
+
+	/* Unsetting to invalidate (all set but first). */
+	cb->connect_request_handler = NULL;
+	assert_equals(JAL_E_INVAL, jaln_register_connection_callbacks(ctx, cb));
+
+	jaln_connection_callbacks_destroy(&cb);
+	jaln_context_destroy(&ctx);
 }
