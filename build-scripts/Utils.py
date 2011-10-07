f91a733a851892b67091f3b1f18ece9da9441563
Phil Black-Knight <pblack@tresys.com>
2011-10-07 14:06:25 -0400

    bug-13395: fixing installed binaries to link to the library names, rather than use the build path for the library.

--- build-scripts/Utils.py
+++ build-scripts/Utils.py
@@ -11,3 +11,15 @@ def recursive_glob(source_dir, pattern):
 		for filename in fnmatch.filter(filenames, pattern):
 			matches.append(os.path.join(root, filename))
 	return matches
+
+def add_project_lib(env, libdir, libname):
+	variant = 'debug'
+	if (env['release']):
+		variant = 'release'
+	env.MergeFlags("-L%s/%s/src/%s/src/ -l%s" % (env['SOURCE_ROOT'], variant, libdir, libname))
+
+def install_for_build(env, dest, target):
+	variant = 'debug'
+	if (env['release']):
+		variant = 'release'
+	env.Default(env.Install("%s/%s/%s" % (env['SOURCE_ROOT'], variant, dest), target))
