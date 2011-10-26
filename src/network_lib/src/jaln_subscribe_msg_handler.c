c1afcfa12910dfb49225a3be383d3eae13ec36a7
Phil Black-Knight <pblack@tresys.com>
2011-10-26 13:02:23 -0400

    bug-13672: adding helper to parse data out of a subscribe message

---
+++ src/network_lib/src/jaln_subscribe_msg_handler.c
@@ -0,0 +1,74 @@
+/**
+ * @file jaln_subscribe_msg_handler.c This file contains the function
+ * definitions for helper functions used to process an 'initialize'
+ * message.
+ *
+ * @section LICENSE
+ *
+ * Source code in 3rd-party is licensed and owned by their respective
+ * copyright holders.
+ *
+ * Copyright (c) 2011 Tresys Technology LLC, Columbia, Maryland, USA
+ *
+ * This software was developed by Tresys Technology LLC
+ * with U.S. Government sponsorship.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <jalop/jaln_network_types.h>
+
+#include "jal_alloc.h"
+
+#include "jaln_subscribe_msg_handler.h"
+#include "jaln_message_helpers.h"
+#include "jaln_strings.h"
+
+enum jal_status jaln_process_subscribe(VortexFrame *frame, char **sid_out)
+{
+	if (!frame || !sid_out || *sid_out) {
+		return JAL_E_INVAL;
+	}
+	enum jal_status ret = JAL_E_INVAL;
+
+	if (!jaln_check_content_type_and_txfr_encoding_are_valid(frame)) {
+		goto err_out;
+	}
+
+	const char *msg = VORTEX_FRAME_GET_MIME_HEADER(frame, JALN_HDRS_MESSAGE);
+	if (!msg) {
+		goto err_out;
+	}
+	if (0 != strcasecmp(msg, JALN_MSG_SUBSCRIBE)) {
+		goto err_out;
+	}
+
+	const char *sid = VORTEX_FRAME_GET_MIME_HEADER(frame, JALN_HDRS_SERIAL_ID);
+	if (!sid) {
+		goto err_out;
+	}
+	*sid_out = jal_strdup(sid);
+	axl_stream_trim(*sid_out);
+	if (0 == strlen(*sid_out)) {
+		goto err_out;
+	}
+	ret = JAL_OK;
+	goto out;
+
+err_out:
+	free(*sid_out);
+	*sid_out = NULL;
+out:
+	return ret;
+}
+
