634cbf24419d54f7b0cbc9c6c3b01fe37c7846f7
Phil Black-Knight <pblack@tresys.com>
2011-11-02 12:17:27 -0400

    bug-13767: unit tests for code to check that a record_info structure is valid.

--- src/network_lib/test/test_jaln_record_info.c
+++ src/network_lib/test/test_jaln_record_info.c
@@ -81,3 +81,79 @@ void test_record_info_destroy_does_not_crash()
 	jaln_record_info_destroy(NULL);
 }
 
+void test_record_info_is_valid_fails_with_bad_type()
+{
+	rec_info->type = 0;
+	assert_false(jaln_record_info_is_valid(rec_info));
+
+	rec_info->type = 1 << 3;
+	assert_false(jaln_record_info_is_valid(rec_info));
+}
+
+void test_record_info_is_valid_fails_with_bad_serial_id()
+{
+	free(rec_info->serial_id);
+	rec_info->serial_id = NULL;
+	assert_false(jaln_record_info_is_valid(rec_info));
+}
+
+void test_record_info_is_valid_fails_without_sys_meta()
+{
+	rec_info->type = JALN_RTYPE_JOURNAL;
+	rec_info->sys_meta_len = 0;
+	assert_false(jaln_record_info_is_valid(rec_info));
+
+	rec_info->type = JALN_RTYPE_AUDIT;
+	assert_false(jaln_record_info_is_valid(rec_info));
+
+	rec_info->type = JALN_RTYPE_LOG;
+	assert_false(jaln_record_info_is_valid(rec_info));
+}
+
+void test_record_info_is_valid_fails_when_missing_app_meta()
+{
+	rec_info->type = JALN_RTYPE_JOURNAL;
+	rec_info->app_meta_len = 0;
+
+	assert_true(jaln_record_info_is_valid(rec_info));
+
+	rec_info->type = JALN_RTYPE_AUDIT;
+	assert_true(jaln_record_info_is_valid(rec_info));
+
+	rec_info->type = JALN_RTYPE_LOG;
+	assert_true(jaln_record_info_is_valid(rec_info));
+}
+
+void test_record_info_is_valid_fails_for_journal_without_payload()
+{
+	rec_info->type = JALN_RTYPE_JOURNAL;
+	rec_info->payload_len = 0;
+	assert_false(jaln_record_info_is_valid(rec_info));
+}
+
+void test_record_info_is_valid_fails_when_record_info_is_null()
+{
+	assert_false(jaln_record_info_is_valid(NULL));
+}
+
+void test_record_info_is_valid_fails_for_audit_without_payload()
+{
+	rec_info->type = JALN_RTYPE_AUDIT;
+	rec_info->payload_len = 0;
+	assert_false(jaln_record_info_is_valid(rec_info));
+}
+
+void test_record_info_is_valid_fails_for_log_without_paylaod_or_app_meta()
+{
+	rec_info->type = JALN_RTYPE_LOG;
+	rec_info->payload_len = 0;
+	rec_info->app_meta_len = 0;
+	assert_false(jaln_record_info_is_valid(rec_info));
+}
+
+void test_record_info_is_valid_works_for_log_without_paylaod()
+{
+	rec_info->type = JALN_RTYPE_LOG;
+	rec_info->payload_len = 0;
+	assert_true(jaln_record_info_is_valid(rec_info));
+}
