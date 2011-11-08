bcec1c35299606a8faa0aeca2e7ac041ab253157
Phil Black-Knight <pblack@tresys.com>
2011-11-08 10:30:05 -0500

    bug-13854: adding unit tests for the hash of sessions maintained by the jaln_ctx

--- src/network_lib/test/test_jaln_context.c
+++ src/network_lib/test/test_jaln_context.c
@@ -27,20 +27,37 @@
  */
 
 #include <jalop/jaln_network.h>
-#include "jaln_context.h"
-
 #include <test-dept.h>
 #include <string.h>
 
+#include "jaln_context.h"
+#include "jaln_session.h"
+
+#define CH_NUM 3
+#define HOSTNAME_1 "192.168.1.1"
+#define HOSTNAME_2 "192.168.1.2"
+
 static jaln_context *ctx = NULL;
+static struct jaln_session *sess = NULL;
+static char *hostname_1 = NULL;
+static char *hostname_2 = NULL;
 void setup()
 {
 	ctx = jaln_context_create();
+
+	sess = jaln_session_create();
+	sess->rec_chan_num = CH_NUM;
+	sess->ch_info->hostname = strdup(HOSTNAME_1);
+	hostname_1 = strdup(HOSTNAME_1);
+	hostname_2 = strdup(HOSTNAME_2);
 }
 
 void teardown()
 {
 	jaln_context_destroy(&ctx);
+	jaln_session_destroy(&sess);
+	free(hostname_1);
+	free(hostname_2);
 }
 
 void test_context_create()
@@ -51,6 +68,7 @@ void test_context_create()
 	assert_equals((void*) NULL, ctx->conn_callbacks);
 	assert_not_equals((void*) NULL, ctx->dgst_algs);
 	assert_not_equals((void*) NULL, ctx->xml_encodings);
+	assert_not_equals((void*) NULL, ctx->sessions_by_conn);
 	assert_equals(1, ctx->ref_cnt);
 	assert_not_equals((void*)NULL, ctx->sha256_digest);
 	assert_not_equals((void*)NULL, ctx->vortex_ctx);
@@ -77,3 +95,38 @@ void test_ref_and_unref_work()
 	// for leaks.
 	ctx = NULL;
 }
+
+void test_add_and_find_session_work() {
+	assert_equals(JAL_OK, jaln_ctx_add_session_no_lock(ctx, sess));
+	struct jaln_session *found = jaln_ctx_find_session_by_rec_channel_no_lock(ctx, hostname_1, CH_NUM);
+	assert_pointer_equals(sess, found);
+
+	found = jaln_ctx_find_session_by_rec_channel_no_lock(ctx, hostname_1, CH_NUM + 1);
+	assert_equals((void*)NULL, found);
+
+	found = jaln_ctx_find_session_by_rec_channel_no_lock(ctx, hostname_2, CH_NUM);
+	assert_equals((void*)NULL, found);
+}
+
+void test_remove_session_works() {
+	assert_equals(JAL_OK, jaln_ctx_add_session_no_lock(ctx, sess));
+	struct jaln_session *found = jaln_ctx_find_session_by_rec_channel_no_lock(ctx, hostname_1, CH_NUM);
+	assert_pointer_equals(sess, found);
+
+	jaln_ctx_remove_session_no_lock(ctx, sess);
+
+	found = jaln_ctx_find_session_by_rec_channel_no_lock(ctx, hostname_1, CH_NUM);
+	assert_pointer_equals((void*)NULL, found);
+
+}
+
+void test_cmp_session_rec_channel_to_channel_works() {
+	int chan = CH_NUM;
+	assert_false(jaln_ctx_cmp_session_rec_channel_to_channel(NULL, &chan));
+	assert_false(jaln_ctx_cmp_session_rec_channel_to_channel(sess, NULL));
+	assert_true(jaln_ctx_cmp_session_rec_channel_to_channel(sess, &chan));
+
+	chan += 1;
+	assert_false(jaln_ctx_cmp_session_rec_channel_to_channel(sess, &chan));
+}
+
