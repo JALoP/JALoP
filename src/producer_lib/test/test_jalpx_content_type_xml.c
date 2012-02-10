95d1ff402a08584fa750e06cf776dde1711af1a9
Michael Napolitano <mnapolitano@tresys.com>
2012-02-10 17:35:43 -0500

    bug-15111: unit tests for libxml2 version of content_type_to_elem

--- src/producer_lib/test/test_jalpx_content_type_xml.c
+++ src/producer_lib/test/test_jalpx_content_type_xml.c
@@ -34,6 +34,7 @@
 
 #include "jalpx_content_type_xml.h"
 #include "jal_alloc.h"
+#include "xml2_test_utils.h"
 
 struct jalp_content_type *ct = NULL;
 
@@ -46,8 +47,10 @@ struct jalp_content_type *ct = NULL;
 #define P3_VALUE "p3_value"
 
 #define CONTENT_TYPE_TAG "Content-Type"
+#define PARAMETER_TAG "Parameter"
 #define MEDIA_TYPE_ATTR_NAME "MediaType"
 #define SUB_TYPE_ATTR_NAME "SubType"
+#define NAME_ATTR_NAME "Name"
 #define APPLICATION "application"
 #define AUDIO "audio"
 #define EXAMPLE "example"
@@ -80,6 +83,469 @@ void teardown()
 	jalp_shutdown();
 }
 
+void test_jalpx_content_type_to_elem_fails_with_bad_input()
+{
+	xmlNodePtr new_elem = NULL;
+	ct->media_type = JALP_MT_VIDEO;
+	enum jal_status ret = jalpx_content_type_to_elem(ct, NULL, &new_elem);
+	assert_equals(JAL_E_XML_CONVERSION, ret);
+	assert_equals(NULL, new_elem);
+
+	ret = jalpx_content_type_to_elem(ct, new_doc, NULL);
+	assert_equals(JAL_E_XML_CONVERSION, ret);
+	assert_equals(NULL, new_elem);
+
+	ret = jalpx_content_type_to_elem(NULL, new_doc, &new_elem);
+	assert_equals(JAL_E_XML_CONVERSION, ret);
+	assert_equals(NULL, new_elem);
+
+	free(ct->subtype);
+	ct->subtype = NULL;
+	ret = jalpx_content_type_to_elem(ct, new_doc, &new_elem);
+	assert_equals(JAL_E_INVAL_CONTENT_TYPE, ret);
+	assert_equals(NULL, new_elem);
+}
+
+void test_jalpx_content_type_to_elem_fails_with_bad_media_type()
+{
+	xmlNodePtr new_elem = NULL;
+	ct->media_type = -1;
+	enum jal_status ret = jalpx_content_type_to_elem(ct, new_doc, &new_elem);
+	assert_equals(JAL_E_INVAL_CONTENT_TYPE, ret);
+	assert_equals(NULL, new_elem);
+
+	ct->media_type = 8;
+	ret = jalpx_content_type_to_elem(ct, new_doc, &new_elem);
+	assert_equals(JAL_E_INVAL_CONTENT_TYPE, ret);
+	assert_equals(NULL, new_elem);
+}
+
+void test_content_type_to_elem_works_with_no_param()
+{
+	xmlNodePtr new_elem = NULL;
+
+	jalp_param_destroy(&ct->params);
+
+	assert_equals(NULL, ct->params);
+
+        enum jal_status ret = jalpx_content_type_to_elem(ct, new_doc, &new_elem);
+
+	assert_equals(JAL_OK, ret);
+	assert_not_equals(NULL, new_elem);
+
+	xmlDocSetRootElement(new_doc, new_elem);
+
+	xmlChar *ret_val = NULL;
+	xmlNodePtr cur_node = NULL;
+
+	assert_equals(1, new_elem != NULL);
+	assert_equals(1, new_doc != NULL);
+	cur_node = new_doc->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST CONTENT_TYPE_TAG));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST MEDIA_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST APPLICATION));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST SUB_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST SUB_TYPE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->xmlChildrenNode;
+	assert_equals(NULL, cur_node);
+
+	assert_equals(0, validate(new_doc, __FUNCTION__, TEST_XML_APP_META_TYPES_SCHEMA, 0));
+}
+
+void test_content_type_to_elem_fails_with_bad_param()
+{
+	xmlNodePtr new_elem = NULL;
+	ct->media_type = JALP_MT_VIDEO;
+	free(ct->params->key);
+	ct->params->key = NULL;
+	enum jal_status ret = jalpx_content_type_to_elem(ct, new_doc, &new_elem);
+	assert_equals(JAL_E_INVAL_PARAM, ret);
+	assert_equals(NULL, new_elem);
+}
+
+
+void test_content_type_to_elem_sets_correct_string_for_media_type_application()
+{
+	xmlNodePtr new_elem = NULL;
+	ct->media_type = JALP_MT_APPLICATION;
+	enum jal_status ret = jalpx_content_type_to_elem(ct, new_doc, &new_elem);
+	assert_equals(JAL_OK, ret);
+	assert_not_equals(NULL, new_elem);
+	xmlDocSetRootElement(new_doc, new_elem);
+
+	xmlChar *ret_val = NULL;
+	xmlNodePtr cur_node = NULL;
+
+	assert_equals(1, new_elem != NULL);
+	assert_equals(1, new_doc != NULL);
+	cur_node = new_doc->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST CONTENT_TYPE_TAG));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST MEDIA_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST APPLICATION));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST SUB_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST SUB_TYPE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_VALUE));
+	xmlFree(ret_val);
+	
+	assert_equals(0, validate(new_doc, __FUNCTION__, TEST_XML_APP_META_TYPES_SCHEMA, 0));
+}
+
+void test_content_type_to_elem_sets_correct_string_for_media_type_audio()
+{
+	xmlNodePtr new_elem = NULL;
+	ct->media_type = JALP_MT_AUDIO;
+	enum jal_status ret = jalpx_content_type_to_elem(ct, new_doc, &new_elem);
+	assert_equals(JAL_OK, ret);
+	assert_not_equals(NULL, new_elem);
+	xmlDocSetRootElement(new_doc, new_elem);
+
+	xmlChar *ret_val = NULL;
+	xmlNodePtr cur_node = NULL;
+
+	assert_equals(1, new_elem != NULL);
+	assert_equals(1, new_doc != NULL);
+	cur_node = new_doc->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST CONTENT_TYPE_TAG));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST MEDIA_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST AUDIO));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST SUB_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST SUB_TYPE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_VALUE));
+	xmlFree(ret_val);
+	
+	assert_equals(0, validate(new_doc, __FUNCTION__, TEST_XML_APP_META_TYPES_SCHEMA, 0));
+}
+
+void test_content_type_to_elem_sets_correct_string_for_media_type_example()
+{
+	xmlNodePtr new_elem = NULL;
+	ct->media_type = JALP_MT_EXAMPLE;
+	enum jal_status ret = jalpx_content_type_to_elem(ct, new_doc, &new_elem);
+	assert_equals(JAL_OK, ret);
+	assert_not_equals(NULL, new_elem);
+	xmlDocSetRootElement(new_doc, new_elem);
+
+	xmlChar *ret_val = NULL;
+	xmlNodePtr cur_node = NULL;
+
+	assert_equals(1, new_elem != NULL);
+	assert_equals(1, new_doc != NULL);
+	cur_node = new_doc->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST CONTENT_TYPE_TAG));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST MEDIA_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST EXAMPLE));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST SUB_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST SUB_TYPE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_VALUE));
+	xmlFree(ret_val);
+	
+	assert_equals(0, validate(new_doc, __FUNCTION__, TEST_XML_APP_META_TYPES_SCHEMA, 0));
+}
+
+void test_content_type_to_elem_sets_correct_string_for_media_type_image()
+{
+	xmlNodePtr new_elem = NULL;
+	ct->media_type = JALP_MT_IMAGE;
+	enum jal_status ret = jalpx_content_type_to_elem(ct, new_doc, &new_elem);
+	assert_equals(JAL_OK, ret);
+	assert_not_equals(NULL, new_elem);
+	xmlDocSetRootElement(new_doc, new_elem);
+
+	xmlChar *ret_val = NULL;
+	xmlNodePtr cur_node = NULL;
+
+	assert_equals(1, new_elem != NULL);
+	assert_equals(1, new_doc != NULL);
+	cur_node = new_doc->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST CONTENT_TYPE_TAG));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST MEDIA_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST IMAGE));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST SUB_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST SUB_TYPE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_VALUE));
+	xmlFree(ret_val);
+	
+	assert_equals(0, validate(new_doc, __FUNCTION__, TEST_XML_APP_META_TYPES_SCHEMA, 0));
+}
+
+void test_content_type_to_elem_sets_correct_string_for_media_type_message()
+{
+	xmlNodePtr new_elem = NULL;
+	ct->media_type = JALP_MT_MESSAGE;
+	enum jal_status ret = jalpx_content_type_to_elem(ct, new_doc, &new_elem);
+	assert_equals(JAL_OK, ret);
+	assert_not_equals(NULL, new_elem);
+	xmlDocSetRootElement(new_doc, new_elem);
+
+	xmlChar *ret_val = NULL;
+	xmlNodePtr cur_node = NULL;
+
+	assert_equals(1, new_elem != NULL);
+	assert_equals(1, new_doc != NULL);
+	cur_node = new_doc->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST CONTENT_TYPE_TAG));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST MEDIA_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST MESSAGE));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST SUB_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST SUB_TYPE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_VALUE));
+	xmlFree(ret_val);
+
+	assert_equals(0, validate(new_doc, __FUNCTION__, TEST_XML_APP_META_TYPES_SCHEMA, 0));
+}
+
+void test_content_type_to_elem_sets_correct_string_for_media_type_model()
+{
+	xmlNodePtr new_elem = NULL;
+	ct->media_type = JALP_MT_MODEL;
+	enum jal_status ret = jalpx_content_type_to_elem(ct, new_doc, &new_elem);
+	assert_equals(JAL_OK, ret);
+	assert_not_equals(NULL, new_elem);
+	xmlDocSetRootElement(new_doc, new_elem);
+
+	xmlChar *ret_val = NULL;
+	xmlNodePtr cur_node = NULL;
+
+	assert_equals(1, new_elem != NULL);
+	assert_equals(1, new_doc != NULL);
+	cur_node = new_doc->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST CONTENT_TYPE_TAG));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST MEDIA_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST MODEL));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST SUB_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST SUB_TYPE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_VALUE));
+	xmlFree(ret_val);
+	
+	assert_equals(0, validate(new_doc, __FUNCTION__, TEST_XML_APP_META_TYPES_SCHEMA, 0));
+}
+
+void test_content_type_to_elem_sets_correct_string_for_media_type_text()
+{
+	xmlNodePtr new_elem = NULL;
+	ct->media_type = JALP_MT_TEXT;
+	enum jal_status ret = jalpx_content_type_to_elem(ct, new_doc, &new_elem);
+	assert_equals(JAL_OK, ret);
+	assert_not_equals(NULL, new_elem);
+	xmlDocSetRootElement(new_doc, new_elem);
+
+	xmlChar *ret_val = NULL;
+	xmlNodePtr cur_node = NULL;
+
+	assert_equals(1, new_elem != NULL);
+	assert_equals(1, new_doc != NULL);
+	cur_node = new_doc->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST CONTENT_TYPE_TAG));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST MEDIA_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST TEXT));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST SUB_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST SUB_TYPE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_VALUE));
+	xmlFree(ret_val);
+	
+	assert_equals(0, validate(new_doc, __FUNCTION__, TEST_XML_APP_META_TYPES_SCHEMA, 0));
+}
+
 void test_content_type_to_elem_sets_correct_string_for_media_type_video()
 {
 	xmlNodePtr new_elem = NULL;
@@ -89,20 +555,47 @@ void test_content_type_to_elem_sets_correct_string_for_media_type_video()
 	assert_not_equals(NULL, new_elem);
 	xmlDocSetRootElement(new_doc, new_elem);
 
-	printf("\nNEW_CONTENT-TYPE\n");
+	xmlChar *ret_val = NULL;
+	xmlNodePtr cur_node = NULL;
 
-	xmlChar *xmlbuff;
-	int buffersize;
+	assert_equals(1, new_elem != NULL);
+	assert_equals(1, new_doc != NULL);
+	cur_node = new_doc->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST CONTENT_TYPE_TAG));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST MEDIA_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST VIDEO));
+	xmlFree(ret_val);
+	ret_val = xmlGetProp(cur_node, BAD_CAST SUB_TYPE_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST SUB_TYPE));
+	xmlFree(ret_val);
 
-	/*
-	* Dump the document to a buffer and print it
-	* for demonstration purposes.
-	*/
-	xmlDocDumpFormatMemory(new_doc, &xmlbuff, &buffersize, 1);
-	printf("%s", (char *) xmlbuff);
+	cur_node = cur_node->xmlChildrenNode;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P1_VALUE));
+	xmlFree(ret_val);
 
-	/*
-	* Free associated memory.
-	*/
-	xmlFree(xmlbuff);
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P2_VALUE));
+	xmlFree(ret_val);
+
+	cur_node = cur_node->next;
+	assert_equals(0, xmlStrcmp(cur_node->name, BAD_CAST PARAMETER_TAG));
+	ret_val = xmlGetProp(cur_node, BAD_CAST NAME_ATTR_NAME);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_NAME));
+	xmlFree(ret_val);
+	ret_val = xmlNodeGetContent(cur_node);
+	assert_equals(0, xmlStrcmp(ret_val, BAD_CAST P3_VALUE));
+	xmlFree(ret_val);
+
+	assert_equals(0, validate(new_doc, __FUNCTION__, TEST_XML_APP_META_TYPES_SCHEMA, 0));
 }
