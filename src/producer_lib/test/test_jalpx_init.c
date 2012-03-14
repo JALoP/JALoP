27aa397df1f42f963bcc5a340c7f04e6107d7240
Jason Dana <jdana@tresys.com>
2012-03-13 20:41:33 -0400

    bug-15334: Unit tests for conversion of jalp_init to libxml2/xmlsec

---
+++ src/producer_lib/test/test_jalpx_init.c
@@ -0,0 +1,37 @@
+/**
+ * @file test_jalp_init.cpp This file contains tests for jalp_init.
+ *
+ * @section LICENSE
+ *
+ * Source code in 3rd-party is licensed and owned by their respective
+ * copyright holders.
+ *
+ * All other source code is copyright Tresys Technology and licensed as below.
+ *
+ * Copyright (c) 2011 Tresys Technology LLC, Columbia, Maryland, USA
+ *
+ * This software was developed by Tresys Technology LLC
+ * with U.S. Government sponsorship.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <jalop/jalpx_context.h>
+
+#include <test-dept.h>
+
+void test_jalp_init_and_shutdown() {
+	enum jal_status r = jalpx_init();
+	assert_equals(JAL_OK, r);
+	jalpx_shutdown();
+}
