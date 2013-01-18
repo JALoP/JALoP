15695bba6a5a5c8364cc8d0e00a3ccc4813681bc
Phil Black-Knight <pblack@tresys.com>
2013-01-18 06:31:30 -0800

    bug-17973: Adding stubbed unit test file for jaldb_traverse, just for a compile test.

---
+++ src/db_layer/test/test_jaldb_traverse.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file test_jaldb_traverse.cpp This file contains functions that traverse the DB.
+ *
+ * @section LICENSE
+ *
+ * Source code in 3rd-party is licensed and owned by their respective
+ * copyright holders.
+ *
+ * All other source code is copyright Tresys Technology and licensed as below.
+ *
+ * Copyright (c) 2013 Tresys Technology LLC, Columbia, Maryland, USA
+ *
+ * This software was developed by Tresys Technology LLC
+ * with U.S. Government sponsorship.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// The test-dept code doesn't work very well in C++ when __STRICT_ANSI__ is
+// not defined. It tries to use some gcc extensions that don't work well with
+// C++.
+
+#ifndef __STRICT_ANSI__
+#define __STRICT_ANSI__
+#endif
+extern "C" {
+#include <test-dept.h>
+}
+
+#include "jaldb_traverse.h"
+
+extern "C" void setup()
+{
+}
+
+extern "C" void teardown()
+{
+}
