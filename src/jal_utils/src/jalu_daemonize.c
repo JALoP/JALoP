69a6e1cd425b4ceaa9b9187a8594dd14896bbdf4
Mollick.Arefin <arefinm@ctc.com>
2022-12-15 23:25:01 -0500

    701, 704 - Add pid_file, log_dir parameters for config files for jald, local-store

--- src/jal_utils/src/jalu_daemonize.c
+++ src/jal_utils/src/jalu_daemonize.c
@@ -27,31 +27,93 @@
  */
 #include "jalu_daemonize.h"
 
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <sys/types.h>
 #include "jal_asprintf_internal.h"
 
-int jalu_daemonize() {
-	pid_t pid, sid;
-	pid = fork();
-	if (pid < 0) {
-		return -1;
-	}
-	if (pid > 0) {
-		//exit the parent process
-		exit(0);
-	}
+int jalu_daemonize(const char *log_dir, const char *pid_file) {
 
-	sid = setsid();
-	if (sid < 0) {
+	// Daemonize the process
+	// This causes the parent to exit, sets the sid for the child process,
+	// changes the directory to "/", and redirects stdin/out/err to /dev/null
+	if(0 != daemon(0,0)) {
 		return -1;
 	}
 
-	int rc = daemon(0, 0);
-	return rc;
+	// There is a known issue with the way daemonization works
+	// in order to prevent the daemonized process from acting as a session
+	// host - and in paticular from potentially attaching to a new
+	// terminal if one were created, an additional fork() call
+	// is recommended.
+	switch(fork()) {
+		case -1: return -1; // fork error
+		case 0: break; // child continues
+		default: exit(0); // parent process exits
+	}
+
+
+	if (log_dir != NULL) {
+		// Re-redirect stdout to a log file in the provided directory
+		char filename[PATH_MAX];
+
+		if (snprintf(filename, sizeof(filename), "%s/stdout.log", log_dir) < 0) {
+			return -1;
+		}
+
+		mode_t mode = 0660; // Let umask decide the group permissions
+		int flags = O_CREAT | O_WRONLY | O_TRUNC;
+
+		// Attempt to create the stdout log file
+		int fd = open(filename, flags, mode);
+		if(-1 == fd) {
+			return -1;
+		}
+
+		// Redirect stdout to the stdout log file
+		if(-1 == dup2(fd, STDOUT_FILENO)) {
+			return -1;
+		}
+		close(fd);
+
+		// Re-redirect stderr to a log file in the provided directory
+		if (snprintf(filename, sizeof(filename), "%s/stderr.log", log_dir) < 0) {
+			return -1;
+		}
+
+		// Attempt to create the stderr log file
+		fd = open(filename, flags, mode);
+		if(-1 == fd) {
+			return -1;
+		}
+
+		// Redirect stderr to the stderr log file
+		if(-1 == dup2(fd, STDERR_FILENO)) {
+			return -1;
+		}
+		close(fd);
+
+		// The stdout stream is buffered. To print immediately, disable buffering on stdout
+		setbuf(stdout, NULL);
+	}
+
+	if (pid_file != NULL) {
+		FILE *fp = fopen(pid_file, "w");
+		if (fp == NULL) {
+			fprintf(stderr, "Failed to open pid file '%s'.\n", pid_file);
+			return -1;
+		}
+
+		fprintf(fp, "%ld\n", (long) getpid());
+		fclose(fp);
+	}
+
+	return 0;
 }
 
 int jalu_pid(const char* path) {
