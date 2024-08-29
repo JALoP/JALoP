c82f7f158923e1a841b166c0e9ed9bbdb424ed38
Mollick A. Arefin <mollick.arefin@cdsc.ctc.com>
2024-08-29 11:12:19 -0400

    Updated installation script.

--- install_rhel_x86_64.sh
+++ install_rhel_x86_64.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 scons install --prefix=/usr --libdir=/usr/lib64 -j4
+
+#Install JALoP schemas
+echo "Installing JALoP schemas to /usr/share/jalop/schemas..."
+rm -rf /usr/share/jalop/schemas/
+mkdir -p /usr/share/jalop/schemas/
+cp -rf ./schemas/* /usr/share/jalop/schemas/  || echo "Error: failed to copy JALoP schemas"
+echo "Successfully installed JALoP schemas."
