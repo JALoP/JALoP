0d707fc32de6986b13b9dbed238d58e253a1d273
Mollick A. Arefin <mollick.arefin@cdsc.ctc.com>
2026-01-21 22:36:58 -0500

    Changes related to SECCOMP configuration and Rust Inline Filter.

--- src/test_utils/scripts/seccomp/create_seccomp_allowed_calls.py
+++ src/test_utils/scripts/seccomp/create_seccomp_allowed_calls.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python
 import os
 import sys
-from pathlib import Path
 import xml.etree.ElementTree as ET
 from datetime import datetime
 
 seccomp_out_dir = sys.argv[1]
 
 now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#command = "echo '" + now + "' > my_rules.txt"
-os.system(command)
 seccomp_out_files = [f for f in os.listdir(seccomp_out_dir) if os.path.isfile(seccomp_out_dir + os.sep + f)]
 for filename in seccomp_out_files:
     out_path = os.path.join(seccomp_out_dir, filename)
