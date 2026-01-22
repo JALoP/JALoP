012b038f0e4edc8f3760f5368566ebcc18ea609d
Mollick A. Arefin <mollick.arefin@cdsc.ctc.com>
2026-01-22 12:19:59 -0500

    Removed rust cargo vendor scripts dependency on .gitlab/utils.sh

--- create_cargo_vendor_files.sh
+++ create_cargo_vendor_files.sh
@@ -11,8 +11,22 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
+function package_cargo_vendor_files() {
+    target_rhel_version=$1
+    vendor_tar=vendor-cargo-$target_rhel_version.tar.gz
+    dnf remove -y rust-*-devel
+    rm -rf /usr/share/cargo/registry/
+    sh src/jaldb_inline_filter/packages.sh $target_rhel_version
+    vendor_dest=vendor-cargo/vendor
+    rm -rf ${vendor_dest}
+    mkdir -p ${vendor_dest}
+    cp -r /usr/share/cargo/registry/* ${vendor_dest}
+    vendor_root=$(dirname ${vendor_dest})
+    tar czf ${vendor_tar} -C ${vendor_root} .
+    du -sh ${vendor_tar}
+}
+
 echo "Begin generating JALoP cargo vendor files...."
-source .gitlab/ci/utils.sh
 
 package_cargo_vendor_files 7
 package_cargo_vendor_files 8
